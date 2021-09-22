e3da268f87e1f0b390f7b8d2d2298678f13b70b8
Rage Uday Kiran <uday.rage@gmail.com>
2021-09-22 09:14:39 +0900

    newMainFile

---
+++ uploadPYPI.sh
@@ -0,0 +1,23 @@
+echo "Uninstall old rasterMiner version"
+pip uninstall -y rasterMiner
+
+echo "Running setup"
+python3 setup.py sdist bdist_wheel
+
+echo "Uploading to test repository"
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+echo "Wait for 5 minute to update the repository"
+sleep 300
+
+echo "installing PAMI from the testPYPI"
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps rasterMiner
+
+echo "Uploading PAMI to main PYPI repository"
+python3 -m twine upload dist/*
+
+echo "Deleting unnecessary files"
+rm -rf dist/ rasterMiner.egg-info/ build/
+
+
+echo "Completed."
